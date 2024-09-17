f5966ecc1207479aae0c24d084fbb4c2ec1112ee
Cage03 <timmermansstijn1@gmail.com>
2024-09-17 11:42:12 +0200

    Minor tweak

--- src/logic/classes/match.ts
+++ src/logic/classes/match.ts
@@ -1,8 +1,8 @@
 ﻿import {ConversationDto, MatchDto} from "../../interfaces/dto";
 import Conversation from "./conversation";
-import { IMatch } from "../../interfaces/classes";
-import { AppDataSource } from "../../data-source";
-import { MatchEntity } from "../../database/entity";  // Assuming you have an entity for Match
+import {IMatch} from "../../interfaces/classes";
+import {AppDataSource} from "../../data-source";
+import {MatchEntity} from "../../database/entity";  // Assuming you have an entity for Match
 
 export default class Match implements IMatch {
     private _conversation?: Conversation;
@@ -18,7 +18,7 @@ export default class Match implements IMatch {
     }
 
     // region CRUD methods
-    
+
     async create(): Promise<void> {
         const matchRepository = AppDataSource.getRepository(MatchEntity);
 
@@ -28,9 +28,9 @@ export default class Match implements IMatch {
         });
 
         await matchRepository.save(match);
-        this._id = match.id;  
+        this._id = match.id;
     }
-    
+
     async read(): Promise<void> {
         if (!this._id) {
             throw new Error("Match ID is undefined");
@@ -38,52 +38,63 @@ export default class Match implements IMatch {
 
         const matchRepository = AppDataSource.getRepository(MatchEntity);
         const match = await matchRepository.findOne({
-            where: { id: this._id },
-            relations: ["conversations"],
+            where: {id: this._id},
+            relations: ["conversations", "conversations.messages", "conversations.messages.user"],
         });
 
         if (!match) {
             throw new Error("Match not found");
         }
-        
-        this._user_id_one = match.user_id_one; 
-        this._user_id_two = match.user_id_two; 
-        
+
+        this._user_id_one = match.user_id_one;
+        this._user_id_two = match.user_id_two;
+
         this._conversation = match.conversations.length
-            ? new Conversation(match.conversations[0])
+            ? new Conversation({
+                id: match.conversations[0].id,
+                match_id: match.id,
+                messages: match.conversations[0].messages.map((message) => ({
+                    id: message.id,
+                    content: message.content,
+                    created_date: message.created_date ?? undefined,
+                    conversation_id: message.conversation?.id,
+                    user_id: message.user?.id ?? undefined,
+                })),
+            })
             : undefined;
     }
-    
+
+
     async update(): Promise<void> {
         if (!this._id) {
             throw new Error("Match ID is undefined");
         }
 
         const matchRepository = AppDataSource.getRepository(MatchEntity);
-        const match = await matchRepository.findOne({ where: { id: this._id } });
+        const match = await matchRepository.findOne({where: {id: this._id}});
 
         if (!match) {
             throw new Error("Match not found");
         }
-        
+
         match.user_id_one = this._user_id_one ?? match.user_id_one;
         match.user_id_two = this._user_id_two ?? match.user_id_two;
 
         await matchRepository.save(match);
     }
-    
+
     async delete(): Promise<void> {
         if (!this._id) {
             throw new Error("Match ID is undefined");
         }
 
         const matchRepository = AppDataSource.getRepository(MatchEntity);
-        const match = await matchRepository.findOne({ where: { id: this._id } });
+        const match = await matchRepository.findOne({where: {id: this._id}});
 
         if (!match) {
             throw new Error("Match not found");
         }
-        
+
         await matchRepository.remove(match);
     }
 
@@ -131,5 +142,6 @@ export default class Match implements IMatch {
     set user_id_two(value: number | undefined) {
         this._user_id_two = value;
     }
+
     // endregion
 }
