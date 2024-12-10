1faf3b5b7f6e91912811a99b3cce1324a0cb2716
T.M. Albregts <153903271+Thomas535855@users.noreply.github.com>
2024-12-10 13:41:11 +0100

    Update index.ts

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -1,9 +1,1 @@
-﻿import postCreate from './post-create'
-import userIdGet from "./userId-get";
-import uploadImage from './upload-image-post';
 
-export{
-    postCreate,
-    userIdGet,
-    uploadImage
-}
